7eb4d4174b47325bdad62a85816667f4db85c665
DIGIT-CORE <vdacosta@voidtek.com>
2018-04-24 11:48:27 +0000

    Deployed 6e793a01293a3316a064aa98e1f64c4de3d4994e 2.5.7 [SKIP CI]

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit809d8e4e201b6518b5f71f1a754d75cd::getLoader();
+return ComposerAutoloaderInitfb91c22eccf2e29476204671cd7e9df7::getLoader();
