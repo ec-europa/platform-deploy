e58b8099ed12b4e8572dbed0c60dd83fa2c9d34a
enriquelacoma <enriquelacoma@gmail.com>
2018-05-25 08:46:39 +0000

    Deployed 3394de0f3502a0d0cf125e6a91ae7a994f1b776b  [SKIP CI]

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit20df5a822f3b65ff68e4546583b55c62::getLoader();
+return ComposerAutoloaderInitd685e2b07de654712f44d6248d2e807a::getLoader();
