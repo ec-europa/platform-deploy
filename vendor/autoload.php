ea9522de0f0d085b67c02a60e888c0540178dfa9
DIGIT-CORE <aritomelo@gmail.com>
2018-10-25 08:48:46 +0000

    Deployed 7f7dab54576c00fac8639a26f1d033f44de4f1f2 2.3.112 [SKIP CI]

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit25b7fa15cf72e78138793b30919972e8::getLoader();
+return ComposerAutoloaderInitc01b7b54078d7f2844ba76c3370ff8a8::getLoader();
