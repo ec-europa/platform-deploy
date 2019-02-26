4e5c5868011c4372f914f9b7d0079c63fb619471
Fefaine <aritomelo@gmail.com>
2019-02-26 12:54:19 +0000

    Deployed 73b0f40dc7a5f50511822c126d45a1743b8ce3eb  [SKIP CI]

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitb8db44d67c9b2fb5304f40cf3143ffe7::getLoader();
+return ComposerAutoloaderInit711b29ae2045137289426963e1352a87::getLoader();
