09d6a6e43774e6efa02204f892c815cdd8c2bcac
DIGIT-CORE <fefaine@gmail.com>
2019-03-22 14:07:14 +0000

    Deployed d8c2c6db93228ff017116bbbd5a0044be5972471 2.5.136.2 [SKIP CI]

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc54fa087b40191721c971afc2c795d56::getLoader();
+return ComposerAutoloaderInit2e28116a3eaca2d0849f70d58a584b5c::getLoader();
