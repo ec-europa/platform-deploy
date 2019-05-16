b98f511266f6dcbce31bce4207a6b37c2df56322
digit-fpfis-devops <fefaine@gmail.com>
2019-05-16 09:32:25 +0000

    Deployed 53fda8826aa78fad000807b57de235cef747e7ad 2.5.137 [SKIP CI]

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitafffe8c588f49201e6225c45d189b763::getLoader();
+return ComposerAutoloaderInitc1ef789ee15915392b82aed54cf69a06::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitafffe8c588f49201e6225c45d189b763
+class ComposerStaticInitc1ef789ee15915392b82aed54cf69a06
 {
     public static $files = array (
         '32dcc8afd4335739640db7d200c1971d' => __DIR__ . '/..' . '/symfony/polyfill-apcu/bootstrap.php',
@@ -169,9 +169,9 @@ class ComposerStaticInitafffe8c588f49201e6225c45d189b763
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitafffe8c588f49201e6225c45d189b763::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitafffe8c588f49201e6225c45d189b763::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInitafffe8c588f49201e6225c45d189b763::$prefixesPsr0;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitc1ef789ee15915392b82aed54cf69a06::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitc1ef789ee15915392b82aed54cf69a06::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitc1ef789ee15915392b82aed54cf69a06::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
