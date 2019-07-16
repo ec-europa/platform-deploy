0143752847560ba26e01ff0f0fe869e2f8bfa55d
Fefaine <fefaine@gmail.com>
2019-07-16 13:31:33 +0000

    Deployed 1211bc2ee8340e31bd31b03ea6d40c9a24554f66  [SKIP CI]

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit2ad475854462e7ded40329456a1ce071::getLoader();
+return ComposerAutoloaderInitdfa94fa1815393fa070bb6f2db2b3583::getLoader();
